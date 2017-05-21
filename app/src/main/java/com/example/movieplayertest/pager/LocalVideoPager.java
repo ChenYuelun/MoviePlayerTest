1f9410123799bdbc0c8667cd4258791d69c0f6ea
ChenYuelun <911172573@qq.com>
2017-05-21 20:57:53 +0800

    ListView点击事件，使用系统播放器播放本地视频

--- app/src/main/java/com/example/movieplayertest/pager/LocalVideoPager.java
+++ app/src/main/java/com/example/movieplayertest/pager/LocalVideoPager.java
@@ -2,6 +2,7 @@ package com.example.movieplayertest.pager;
 
 
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Handler;
@@ -9,6 +10,7 @@ import android.os.Message;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -31,23 +33,32 @@ public class LocalVideoPager extends BaseFragment {
 
     @Override
     public View initView() {
-        View view = View.inflate(context, R.layout.local_video_item,null);
+        View view = View.inflate(context, R.layout.local_video_item, null);
         lv_local_video = (ListView) view.findViewById(R.id.lv_local_video);
         tv_nodata = (TextView) view.findViewById(R.id.tv_nodata);
+
+        lv_local_video.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.parse(mediaItems.get(position).getData()), "video/*");
+                startActivity(intent);
+            }
+        });
         return view;
 
     }
 
-    private Handler handler = new Handler(){
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            if(mediaItems != null && mediaItems.size() > 0) {
+            if (mediaItems != null && mediaItems.size() > 0) {
                 tv_nodata.setVisibility(View.GONE);
-                adapter = new LocalVideoAdapter(context,mediaItems);
+                adapter = new LocalVideoAdapter(context, mediaItems);
                 lv_local_video.setAdapter(adapter);
 
-            }else {
+            } else {
                 tv_nodata.setVisibility(View.VISIBLE);
             }
 
@@ -64,8 +75,8 @@ public class LocalVideoPager extends BaseFragment {
     }
 
     private void getLocalVideoData() {
-        new Thread(){
-            public void run(){
+        new Thread() {
+            public void run() {
                 mediaItems = new ArrayList<MediaItem>();
                 ContentResolver resolver = context.getContentResolver();
                 Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
@@ -77,20 +88,19 @@ public class LocalVideoPager extends BaseFragment {
 
                 };
                 Cursor cursor = resolver.query(uri, objs, null, null, null);
-                if(cursor != null) {
-                    while(cursor.moveToNext()) {
+                if (cursor != null) {
+                    while (cursor.moveToNext()) {
                         String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
                         long duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
                         long size = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.SIZE));
                         String data = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
-                        Log.e("TAG","name=" + name);
-                        mediaItems.add(new MediaItem(name,duration,size,data));
+                        Log.e("TAG", "name=" + name);
+                        mediaItems.add(new MediaItem(name, duration, size, data));
 
 
                     }
                     cursor.close();
                     handler.sendEmptyMessage(1);
-
 
 
                 }
