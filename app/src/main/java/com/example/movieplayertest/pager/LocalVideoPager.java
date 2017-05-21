0999481a573b69d671845872de91b156bd832605
ChenYuelun <911172573@qq.com>
2017-05-21 20:35:26 +0800

    基类Fragment及四个页面简单编写完成，监听RadioGroup点击并切换相应页面完成，解决重复加载页面的问题

---
+++ app/src/main/java/com/example/movieplayertest/pager/LocalVideoPager.java
@@ -0,0 +1,33 @@
+package com.example.movieplayertest.pager;
+
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.movieplayertest.fragment.BaseFragment;
+
+/**
+ * Created by chenyuelun on 2017/5/21.
+ */
+
+public class LocalVideoPager extends BaseFragment {
+    private TextView textView;
+    @Override
+    public View initView() {
+        textView = new TextView(context);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(Color.RED);
+        textView.setTextSize(30);
+        return textView;
+    }
+
+
+    @Override
+    public void initDatas() {
+        super.initDatas();
+        textView.setText("这是本地视频界面");
+
+    }
+}
