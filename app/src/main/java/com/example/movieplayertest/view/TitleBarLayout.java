36fa943673ac22f8b950069f4c6f61cf643214e3
ChenYuelun <911172573@qq.com>
2017-05-21 20:16:07 +0800

    顶部布局完成，图片资源加载

---
+++ app/src/main/java/com/example/movieplayertest/view/TitleBarLayout.java
@@ -0,0 +1,16 @@
+package com.example.movieplayertest.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by chenyuelun on 2017/5/21.
+ */
+public class TitleBarLayout extends LinearLayout {
+
+    public TitleBarLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
